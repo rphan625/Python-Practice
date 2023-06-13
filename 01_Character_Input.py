a5625805df908652429a4da608d518e84a02e128
Richard Phan <richardphan625@gmail.com>
2023-06-13 15:33:13 -0700

    Add files via upload

---
+++ 01_Character_Input.py
@@ -0,0 +1,19 @@
+""" Exercise 1 Character Input
+ ask a user for name and age, tell them when they will turn 100 years old
+"""
+
+from datetime import datetime
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+till100 = 100 - age
+currentyear = datetime.now().year
+
+msg = "You will be 100 at year " + str(currentyear + till100)
+print(msg)
+
+repeat = int(input("Enter a number: "))
+
+for _ in range(repeat):
+    print(msg)
