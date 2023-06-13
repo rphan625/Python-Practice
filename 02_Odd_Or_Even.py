c8add78ae0779caa01537b3b608cc18a28c8c056
rphan625 <richardphan625@gmail.com>
2023-06-13 15:14:41 -0700

    first commit

---
+++ 02_Odd_Or_Even.py
@@ -0,0 +1,20 @@
+""" Exercise 2
+    Ask the user for number, print if even or odd
+    Extras:
+        If divisible by 4
+        Ask user for 2 nums, one called num, 2nd to divide by, print
+        if it divides evenly or not"""
+
+num = int(input("Enter a number: "))
+check = int(input("Enter second number: "))
+def EvenOrOdd(number, check):
+    if check == 0:
+        print("You cannot divide by 0")
+    if number % 4 == 0:
+        print("Divisible by 4")
+    elif number % check == 0:
+        print(f"{num} divides evenly with {check}!")
+    else:
+        print(f"{num} doesn't divide evenly with {check}")
+
+EvenOrOdd(num, check)
